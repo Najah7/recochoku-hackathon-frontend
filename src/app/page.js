d74dffd7d408c5598f716f283ae5aac5ce569564
Najah7 <najahmatsuo7@gmail.com>
2024-03-24 13:50:32 +0900

    🎉 [Add] Next.js project

---
+++ src/app/page.js
@@ -0,0 +1,9 @@
+import CustomCard from "./components/custom-card";
+
+export default function Card() {
+  return (
+    <main className="flex justify-center items-center h-screen">
+      This is a Home Page
+    </main>
+  );
+}
